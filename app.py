66c1f2dd4c329390d7408b457c3bd18be534bdc2
Shilpi Sinha <68265036+Shilpi-sinha@users.noreply.github.com>
2020-08-11 20:10:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 model = pickle.load(open('C:/Users/hp/diabetes_model.pkl', 'rb'))
 
 
-@app.route('/home')
+@app.route('/')
 def home():
     return render_template('home.html')
 
